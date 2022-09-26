3f3cd1436ca3a4582629a55caaa1375d70d268c7
Mauricio <mauricio_cy@hotmail.com>
2022-09-26 17:31:52 -0300

    Hash

--- api/controllers/UserPassword.js
+++ api/controllers/UserPassword.js
@@ -1,14 +1,23 @@
 const database = require('../models');
 const schema = require('./passwordValidation');
+const bcrypt = require('bcrypt');
 
 class UserPassword {
     static async createPassword(req, res) {
         try {
-            //await schema.validate(req.body);
+            await schema.validate(req.body);
             const newPassword = req.body;
             const { id } = req.params;
-            await database.users.update(newPassword, { where: { id: Number(id) }});
-            return res.status(200).json({message: `Password created successfully for the user's id: ${id}`});
+            const previousPassword = await database.users.findAll({ where: { id: Number(id) } });
+            const salt = 10;
+            const encryptedPassword = await bcrypt.hash(newPassword.password, salt);
+            if (previousPassword[0].dataValues.password !== null) {
+                return res.status(400).json({message: "This user already have a password"});
+            } else {
+                await database.users.update({ password: encryptedPassword }, { where: { id: Number(id) }});
+                console.log(encryptedPassword);
+                return res.status(200).json({message: "Password created successfully"});
+            }
         } catch (error) {
             return res.status(400).json(error.message);
         }
